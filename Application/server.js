ec99634ed91fecb19a9ded596a19bd3a0202fefc
Soham Shendre <sohamshendre04@gmail.com>
2021-12-04 15:56:40 +0530

    serve static file using express

--- Application/server.js
+++ Application/server.js
@@ -1,26 +1,10 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 8092;
-var path = require('path');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
 
-var server = http.createServer(function (req, res) {
-  fs.readFile(path.join(__dirname, 'dist', 'index.html'), function (err, data) {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Content-Length': data.length,
-    });
-    res.write(data);
-    res.end();
-  });
+app.use(express.static('dist'));
+
+app.listen(port, () => {
+  console.log('server started');
 });
-
-exports.listen = function () {
-  server.listen.apply(server, arguments);
-};
-
-exports.close = function (callback) {
-  server.close(callback);
-};
-
-server.listen(port);
